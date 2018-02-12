25397a669322282da3035a4288d45b1fdb6676da
abhishek371 <kamalabhishek20@gmail.com>
2018-02-12 20:13:57 +0530

    Insertion Sort in Python

---
+++ Sorting/Insertion_Sort/insertion_sort.py
@@ -0,0 +1,31 @@
+def insertionSort(list,n):
+	for i in range(1,n):
+		key=list[i]
+		j=i-1
+		while j>=0 and key<list[j]:
+			list[j+1]=list[j]
+			j=j-1
+		list[j+1]=key
+	print("Sorted List : ",list)
+	
+
+
+
+
+
+
+def main():
+	list=[]
+	print("Enter the numbers in the list(enter x to stop) : ")
+	while True:
+		n=input()
+		if n=='x':
+			break
+		list.append(int(n))
+	insertionSort(list,len(list))
+
+
+
+
+if __name__ == '__main__':
+	main()
