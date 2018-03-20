544b1af8de27a1d5c288fc333096a6e51248bee8
abhishek371 <kamalabhishek20@gmail.com>
2018-03-21 01:23:57 +0530

    File added

---
+++ Greedy_Algorithm/Min_removal.py
@@ -0,0 +1,33 @@
+#Minimum removals to make array sum even
+def Min_removal(num):
+	count_odd=0
+	sum=0
+	for i in num:
+		if i%2!=0:
+			count_odd=count_odd+1
+		sum=sum+i
+	if sum%2!=0:
+		return 1
+	else:
+		return 0
+
+
+
+
+
+
+def main():
+	l=[]
+	print("Enter the numbers u want to add :")
+	while True:
+		l.append(int(input()))
+	val=Min_removal(l)
+	print("Minimum number required : %s"%(val))
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
