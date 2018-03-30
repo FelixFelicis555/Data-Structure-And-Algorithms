b57fe6ed1af09b99533a025945b36919b6e4f3eb
abhishek371 <kamalabhishek20@gmail.com>
2018-03-30 23:53:26 +0530

    Fractional Knapsack Problem

--- Greedy_Algorithm/KnapSack.py
+++ Greedy_Algorithm/KnapSack.py
@@ -19,39 +19,51 @@ class KnapSack:
 	def add_items(self,node):
 		self.items.append(node)
 
-	def printitems(self):
+
+	def sort_nodes(self):
+		self.items.sort(key=lambda x : x.ratio,reverse=True)
+
+	def Total_Profit(self,n,W):
+		self.prof=0
+		self.curr_weight=0
+
+		'''for i in range(n):
+			if(self.curr_weight+self.items[i].weight<=W):
+				self.prof=self.prof+self.items[i].value
+				self.curr_weight=self.curr_weight+self.items[i].weight
+			else:
+				self.prof=self.prof+self.items[i].value*(W-self.curr_weight)/self.items[i].weight
+				break
+		return self.prof'''
+		
+		self.k=W
 		for i in range(n):
-			print(self.items.weight)
-
-
+			if self.k-self.items[i].weight>=0:
+				self.prof=self.prof+self.items[i].value
+				self.k=self.k-self.items[i].weight
+			else:
+				self.prof=self.prof+self.items[i].value*(self.k)/self.items[i].weight
+				break
+		return self.prof
 
 
 def main():
 	n=int(input("Enter the total numbers of items :"))
+	W=int(input("Enter the capacity of knapsack :"))
 	K=KnapSack(n)
-	print("Enter the weights,values of the item : ")
+	print("Enter the weights,values of the item ")
 	for i in range(n):
 		weight=int(input("Enter the weight of item %s :"%(i+1)))
 		value=int(input("Enter the value of item %s :"%(i+1)))
 		N=Node(value,weight,i+1)
 		K.add_items(N)
-
-
-
-if __name__ == '__main__':
-	main()
-
-
-
-
-
-
-
-
-
+	K.sort_nodes()
+	profit=K.Total_Profit(n,W)
+	print("Total profit is %s "%(profit))
 
 
 
 
 if __name__ == '__main__':
 	main()
+
