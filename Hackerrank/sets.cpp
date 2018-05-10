7f5108bc55e47137b0f431bd299dfd272a9e75c2
abhishek371 <kamalabhishek20@gmail.com>
2018-05-11 00:24:19 +0530

    File added

--- Hackerrank/sets.cpp
+++ Hackerrank/sets.cpp
@@ -3,41 +3,39 @@
 #include <vector>
 #include <iostream>
 #include <set>
-#include <map>
 #include <algorithm>
 using namespace std;
 
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    
-    int Q,type,Y;
-    char X[100];
-    map<string,int>student;
+    int Q,y,x;
+    set<int>s;
     cin>>Q;
     for(int i=0;i<Q;i++){
-        cin>>type;
-        if (type==1){
-            cin>>X>>Y;
-            map<string,int>::iterator itr=student.find(X);
-            if(itr==student.end()){
-            student.insert(make_pair(X,Y));}
-            else{
-                int a=student[X];
-                student.insert(make_pair(X,a+Y));
-            }
-            
+        cin>>y;
+        if(y==1){
+            cin>>x;
+            s.insert(x);
             
         }
-        else if (type==2){
-            student.erase(X);
+        else if(y==2){
+            cin>>x;
+            s.erase(x);
             
         }
         else{
-            cout<<student[X];
+            cin>>x;
+            set<int>::iterator itr=s.find(x);
+            if(itr!=s.end())
+            {
+                cout<<"Yes"<<endl;
+            }
+            else{
+                cout<<"No"<<endl;
+            }
             
         }
-        
     }
     return 0;
 }
